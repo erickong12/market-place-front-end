f6f9c23fbc6ad6fa97174e4e035147dbfda040fd
Eric Agustian <eric.agustian@inticorp.tech>
2025-08-23 23:30:09 +0700

    fix cors

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,24 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
 
   return {
     server: {
-      port: parseInt(env.PORT || '5173')
+      port: parseInt(env.PORT || '5173'),
+      host: true
+    },
+    preview: {
+      port: parseInt(env.PORT || '5173'),
+      host: true,
+      allowedHosts: [
+        "*"
+      ]
     },
     plugins: [
       react(),
       tailwindcss(),
     ],
-  };
-});
+  }
+})
